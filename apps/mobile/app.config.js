c8dcab50a1f5816fc388bb59acd27ff4ec3febad
Aryan Motgi <aryanmotgi@Aryans-MacBook-Air.local>
2026-05-04 00:51:28 -0700

    fix(eas): re-add @babel/runtime explicit dep + fix app.config.js projectId

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,4 +1,10 @@
 module.exports = ({ config }) => ({
   ...config,
   runtimeVersion: '1.0.0',
+  extra: {
+    ...config.extra,
+    eas: {
+      projectId: '845c42cd-33e6-42d0-8189-59131144999f',
+    },
+  },
 });
